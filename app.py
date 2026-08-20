6ac129f71c0af4dc88ced41c4eb7a6cdc689503a
Samyog Panthi <samyogpanthee238@gmail.com>
2026-08-20 10:11:56 +0545

    Update app.py

--- app.py
+++ app.py
@@ -3,18 +3,21 @@ from models import db, User
 from flask_login import LoginManager
 import secrets
 from werkzeug.security import generate_password_hash
+from auth_bp import auth_bp
+from views_bp import views_bp
+from admin_bp import admin_bp
+
 
 app = Flask(__name__)
 
-# ---------------- CONFIG ----------------
 app.config['SECRET_KEY'] = secrets.token_hex(32)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# ---------------- DATABASE ----------------
+
 db.init_app(app)
 
-# ---------------- LOGIN MANAGER ----------------
+
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
 login_manager.init_app(app)
@@ -23,20 +26,17 @@ login_manager.init_app(app)
 def load_user(user_id):
     return db.session.get(User, int(user_id))
 
-# ---------------- BLUEPRINTS ----------------
-from auth_bp import auth_bp
-from views_bp import views_bp
-from admin_bp import admin_bp
+
 
 app.register_blueprint(auth_bp, url_prefix='/auth')
 app.register_blueprint(views_bp)
 app.register_blueprint(admin_bp, url_prefix='/admin')
 
-# ---------------- CREATE TABLES & DEFAULT ADMIN ----------------
+
 with app.app_context():
     db.create_all()
 
-    # Create default admin if not exists
+
     admin = User.query.filter_by(email='admin@example.com').first()
     if not admin:
         new_admin = User(
